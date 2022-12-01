40ffe0b71bd159f343e79dfd3b5c473533104e7a
Karthik2008 <bkp.karthi@gmail.com>
2022-12-01 23:44:51 +0530

    initial commit

---
+++ nodejs/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install nodejs -y
+
+sudo apt install npm -y
+
+mkdir ~/first-node-project
+cd ~/first-node-project
+cat <<EOF >app.js
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log('Server running at http://${hostname}:${port}/');
+});
+EOF
+
+nohup node ~/first-node-project/app.js
