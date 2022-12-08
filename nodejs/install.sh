332e9ea78fb3a6b56d8fa4ee9fc5df8f5a9a1c96
Karthik <62302028+Karthik2008@users.noreply.github.com>
2022-12-08 20:33:17 +0530

    added h1 tag

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -14,8 +14,8 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('<h1>Hello World</h1>');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Welcom to RootEdu</h1>');
 });
 
 server.listen(port, hostname, () => {
