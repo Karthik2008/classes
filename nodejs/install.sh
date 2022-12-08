370349ff4b36e23c00113f27a2dfada1a8913e46
Karthik <62302028+Karthik2008@users.noreply.github.com>
2022-12-08 20:39:53 +0530

    Update install.sh

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -15,7 +15,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Welcom to RootEdu</h1>');
+  res.end('<h1>Welcome to RootEdu</h1>');
 });
 
 server.listen(port, hostname, () => {
